4bd3117bc93c6cf89b527a2986b8572955193ec2
Şerif <gungoronline@gmail.com>
2017-09-26 13:58:27 +0300

    Update Baslangic.java

--- HaftaiciOglen/25.09.2017/OOP_KapsullemeOrnek3/Baslangic.java
+++ HaftaiciOglen/25.09.2017/OOP_KapsullemeOrnek3/Baslangic.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 
 public class Baslangic {
 
@@ -5,7 +7,37 @@ public class Baslangic {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Insan insan = new Insan();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("İsim giriniz");
+		insan.setIsim(sc.next());
+		
+		System.out.println("Cinsiyet giriniz (e/k)");
+		char cinsiyet = sc.next().toLowerCase().charAt(0);
+		insan.setCinsiyet(cinsiyet);
+		
+		System.out.println("Medeni durumu (evli/bekar)");
+		char medeniDurum = sc.next().toLowerCase().charAt(0);
+		if (medeniDurum=='e') {
+			insan.setEvliMi(true);
+		}else if(medeniDurum=='b'){
+			insan.setEvliMi(false);
+		}
+		
+		System.out.println("Yaş giriniz");
+		insan.setYas(sc.nextInt());
+		
+		System.out.println("Boy uzunluğu giriniz");
+		insan.setBoy(sc.nextInt());
+		
+		System.out.println("Kilo giriniz");
+		insan.setKilo(sc.nextInt());
+		
+		insan.getTumKisiDetaylari();
+		
+		
 
 	}
 

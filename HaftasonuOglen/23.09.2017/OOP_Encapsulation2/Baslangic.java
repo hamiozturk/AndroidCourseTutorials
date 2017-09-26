118ed8b7b930fb28bf0216971a99ad6186f4c1ef
Şerif <gungoronline@gmail.com>
2017-09-26 14:02:27 +0300

    Update Baslangic.java

--- HaftasonuOglen/23.09.2017/OOP_Encapsulation2/Baslangic.java
+++ HaftasonuOglen/23.09.2017/OOP_Encapsulation2/Baslangic.java
@@ -5,8 +5,8 @@ public class Baslangic {
 	public static void main(String[] args) {
 		Insan insan = new Insan();
 		insan.setYas(10);
-		insan.setIsım("Adem");
-		System.out.println(insan.getIsım());
+		insan.setIsim("Adem");
+		System.out.println(insan.getIsim());
 	}
 
 }
